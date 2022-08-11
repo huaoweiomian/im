1c1f35c1bd2b7e8cc7971cb5d336ef2e39c76e84
huaoweiomian <huaoweiomian@163.com>
2022-08-12 01:03:02 +0800

    add client for net lib

--- baselib/net/channel_client.cpp
+++ baselib/net/channel_client.cpp
@@ -2,5 +2,44 @@
 
 CHANNEL_CLIENT::CHANNEL_CLIENT()
 {
+    sockfd = emptyfd_c;
+}
 
+bool CHANNEL_CLIENT::init(std::string &ip, int port)
+{
+    //1.创建tcp监听套接字
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(-1 == sockfd){
+        return false;
+    }
+    struct sockaddr_in my_addr;
+    memset(&my_addr,0, sizeof(my_addr));
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(port);
+    my_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+
+    if(connect(sockfd, (struct sockaddr*)&my_addr,sizeof(my_addr)) < 0){
+        return false;
+    }
+    return true;
+}
+
+CHANNEL_CLIENT::~CHANNEL_CLIENT()
+{
+    mycolse();
+}
+
+void CHANNEL_CLIENT::mycolse()
+{
+    if(sockfd == emptyfd_c)
+        return;
+    close(sockfd);
+    sockfd = emptyfd_c;
+}
+
+
+
+CHANNEL_CLIENT::operator int()
+{
+    return sockfd;
 }
--- baselib/net/channel_client.h
+++ baselib/net/channel_client.h
@@ -1,11 +1,34 @@
 #ifndef CHANNEL_CLIENT_H
 #define CHANNEL_CLIENT_H
-
-
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>   //inet_addr,inet_aton
+#include <unistd.h>        //read,write,close(fd)
+#include <string.h>
+#include <string>
+#include <iostream>
+using std::string;
+using std::cout;
+using std::endl;
+#include "channel.h"
+#include <sys/epoll.h>
+#include <pthread.h>
+#include "chlq.h"
+#include <string>
+using std::string;
+//线程不安全
+const int emptyfd_c = -1;
 class CHANNEL_CLIENT
 {
 public:
     CHANNEL_CLIENT();
-};
+public:
+    bool init(string& ip, int port);
+    operator int();
+    ~CHANNEL_CLIENT();
+    void mycolse();
+private:
+    int sockfd;
 
+};
 #endif // CHANNEL_CLIENT_H
